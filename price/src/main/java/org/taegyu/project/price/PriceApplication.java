b4fbb07aae1fdd27d314492fc1fbc50f6e1f9a2c
Tae gyu Choi <suker800@gmail.com>
2025-05-04 19:29:38 +0900

    멀티 모듈로 변경

---
+++ price/src/main/java/org/taegyu/project/price/PriceApplication.java
@@ -0,0 +1,13 @@
+package org.taegyu.project.price;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PriceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PriceApplication.class, args);
+    }
+
+}
---
+++ price/src/test/java/org/taegyu/project/price/PriceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.taegyu.project.price;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PriceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
